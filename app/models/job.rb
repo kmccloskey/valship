9af9d3b0ac14a3261b3bd618f3a20b30876022b0
Kyle McCloskey <mccloskeykyle@gmail.com>
2016-01-18 23:03:31 -0500

    Working origin and destination

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,13 +2,23 @@ class Job < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :boat
 
-def o
-	Boat::LOCATIONS[self.origin]
-end
+# I'm checking to see if te origin value is nil here, it looks like a few jobs have a nil origin
+	def text_origin
+		# puts "Is this working *******************************"
+		if self.origin != nil
+			Boat::LOCATIONS[self.origin]
+		else
+			"ORIGIN NOT SET"
+		end
+	end
 
-def d
-	Boat::DESTINATIONS[self.destination]
-end
+	def dog
+		puts ("woof")
+	end
+
+	def dest
+		Boat::DESTINATIONS[self.destination]
+	end
 
 	
 end
